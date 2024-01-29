ccf3bba59867e5616d10592d1dce411346a5f90c
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2024-01-29 08:40:43 +0200

    Time: 115 ms (10.70%), Space: 17.4 MB (57.06%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,37 +1,12 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # Initialize an empty stack to store intermediate results.
-        stack = []
-
-        # Iterate through each token in the RPN expression.
-        for token in tokens:
-            # Check if the token is an addition operator.
-            if token == '+':
-                # Pop the top two elements from the stack.
-                a, b = stack.pop(), stack.pop()
-                # Perform addition and push the result back onto the stack.
-                stack.append(a + b)
-            # Check if the token is a subtraction operator.
-            elif token == '-':
-                # Pop the top two elements from the stack.
-                a, b = stack.pop(), stack.pop()
-                # Perform subtraction and push the result back onto the stack.
-                stack.append(b - a)
-            # Check if the token is a multiplication operator.
-            elif token == '*':
-                # Pop the top two elements from the stack.
-                a, b = stack.pop(), stack.pop()
-                # Perform multiplication and push the result back onto the stack.
-                stack.append(b * a)
-            # Check if the token is a division operator.
-            elif token == '/':
-                # Pop the top two elements from the stack.
-                a, b = stack.pop(), stack.pop()
-                # Perform integer division and push the result back onto the stack.
-                stack.append(int(b / a))
+        num_stack = tokens[:2]
+        for token in tokens[2:]:
+            if token.strip('-').isnumeric():
+                num_stack.append(token)
             else:
-                # The token is an operand, so convert it to an integer and push onto the stack.
-                stack.append(int(token))
-                
-        # The final result will be on the top of the stack.
-        return stack.pop()
+                n1, n2 = num_stack.pop(),  num_stack.pop()
+                res = int(eval(n2 + token + n1))
+                print(f"{n2} {token} {n1} = {res}")
+                num_stack.append(str(res))
+        return int(num_stack.pop())
