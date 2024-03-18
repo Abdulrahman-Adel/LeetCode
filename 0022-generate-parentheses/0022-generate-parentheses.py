2de595508d82d1f645425b316954d13339ae6c85
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2024-03-19 00:46:25 +0200

    Time: 37 ms (61.53%), Space: 16.8 MB (44.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def generateParent(self, s: str, n_open: int, n_close: int) -> None:
         # backtracking algorithm
-        # brute force [try every possiblity and if the next step doesn't work go back (backtrack) to the previous step]
+        # brute force [try every possiblity and if the next step doesn't work go back                   (backtrack) to the previous step]
         # recursion
         open_brackets = '(' * n_open
         close_brackets = ')' * n_close
@@ -18,9 +18,24 @@ class Solution:
                 self.generateParent(s + "(", n_open - 1, n_close)
             if close_brackets:
                 self.generateParent(s + ")", n_open, n_close - 1)
+                
     def generateParenthesis(self, n: int) -> List[str]:
-        self.result = []
+        result = []
+        stack = []
         
-        self.generateParent('(', n-1, n)
-        
-        return self.result
+        # more efficien solution
+        def backtrack(n_open:int, n_close):
+            if n_open == n_close == n:
+                result.append("".join(stack))
+                return 
+            if n_open < n:
+                stack.append('(')
+                backtrack(n_open + 1, n_close)
+                stack.pop()
+            if n_close < n_open:
+                stack.append(')')
+                backtrack(n_open, n_close + 1)
+                stack.pop()
+                
+        backtrack(0, 0)
+        return result
